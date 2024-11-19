e6a6e4aba4e7c01c6767241460ea3584443b3fa7
greatjay1 <accessjay11@yahoo.com>
2024-11-19 15:07:11 +0100

    creating element instances

--- JS/script.js
+++ JS/script.js
@@ -2,35 +2,64 @@
 function getJobs(){
 
 let userInput = document.querySelector(".input-form").value;
-let jobTitle =  document.querySelector(".job-title");
+let jobs =  document.querySelector("#jobs");
+let ErrorD = document.querySelector(".pError");
 
+
+// fetch(`https://api.lmiforall.org.uk/api/v1/vacancies/search?keywords=Digital%20${userInput}`)
 
 fetch(`https://api.lmiforall.org.uk/api/v1/vacancies/search?keywords=Digital%20${userInput}`)
 .then(response => response.ok ? response.json() : Promise.reject("Error fetching API"))
 .then(data =>{
 
-    var json = JSON.parse(data);
-    jobTitle.innerHTML = json["name"];
 
-    console.log(json["name"]);
+
+  
+
+
+
+    for (let index = 0; index < 10; index++) {
+
+        // const jobTitle = data[index].title;
+
+        console.log(data[index].title);
+
+
+
+        jobs.innerHTML = `
+        <div class="container result-page">
+            <div class="row">
+        <div class="col-md-2"><img src="Images/job-list1.png" alt="" srcset=""></div>
+        <div class="col-md-7">
+        
+        <h2 class="job-title">${data[index].title}</h2>
+        
+        <i class="fa-solid fa-briefcase"></i> Creative Agency   <span class="pay-details"> <i class="fa-solid fa-location-dot"></i> Athens, Greece   </span>  <i class="fa-solid fa-sack-dollar"></i> $3500 - $4000
+        
+        </div>
+        <div class="col-md-3">
+        
+            <button class="result-btn">Apply for Job</button>
+            <h6> 7 hours ago</h6>
+        </div>
+            </div>
+        </div>`;
+
+document.body.appendChild(jobs);
+        
+             
+    }
+
+    
 
 })
 
 .catch(() =>{
 
-    jobTitle.innerHTML = "Unable to display";
+    ErrorD.innerHTML = " keyword not found, please try another one"
 
 });
 
-
-
-// let theMain = data
-
-// for (let index = 0; index < data.length; index++) {
-//     const element = data[index];
-    
-// }
-
-
-
 }
+
+
