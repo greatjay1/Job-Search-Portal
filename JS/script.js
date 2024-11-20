e588a9c96168e30ea4dd3c60cff945cb11c513ca
greatjay1 <accessjay11@yahoo.com>
2024-11-20 12:10:58 +0100

    Vacancy page onclick issue

--- JS/script.js
+++ JS/script.js
@@ -14,19 +14,15 @@ fetch(`https://api.lmiforall.org.uk/api/v1/vacancies/search?keywords=Digital%20$
 
 
 
-  
-
-
-
-    for (let index = 0; index < 10; index++) {
-
+     for (let index = 0; index < 8; index++) {
+        
         // const jobTitle = data[index].title;
 
         console.log(data[index].title);
 
 
 
-        jobs.innerHTML = `
+        jobs.innerHTML += `
         <div class="container result-page">
             <div class="row">
         <div class="col-md-2"><img src="Images/job-list1.png" alt="" srcset=""></div>
@@ -45,9 +41,8 @@ fetch(`https://api.lmiforall.org.uk/api/v1/vacancies/search?keywords=Digital%20$
             </div>
         </div>`;
 
-document.body.appendChild(jobs);
-        
-             
+          
+      
     }
 
     
@@ -63,3 +58,76 @@ document.body.appendChild(jobs);
 }
 
 
+// let adminn = document.querySelector(".admin");
+
+
+
+const Administrative = 'Administrative';
+
+
+
+function getVacancies(Administrative){
+
+
+    let err = document.querySelector(".errDisplay");
+    let FilterJobs = document.getElementById("FilterJobs");
+
+    
+    
+    fetch(`https://api.lmiforall.org.uk/api/v1/vacancies/search?keywords=${Administrative}`)
+    .then(response => response.ok ? response.json() : Promise.reject("Error fetching API"))
+    .then(data =>{
+       
+    
+         for (let index = 0; index < 5; index++) {
+            
+            // const jobTitle = data[index].title;
+    
+            console.log(data);
+
+    
+    
+    
+            FilterJobs.innerHTML += `
+            <div class="container result-page">
+                <div class="row">
+            <div class="col-md-2"><img src="Images/job-list1.png" alt="" srcset=""></div>
+            <div class="col-md-7">
+            
+            <h2 class="job-title">${data[index].title}</h2>
+            
+            <i class="fa-solid fa-briefcase"></i> Creative Agency   <span class="pay-details"> <i class="fa-solid fa-location-dot"></i> Athens, Greece   </span>  <i class="fa-solid fa-sack-dollar"></i> $3500 - $4000
+            
+            </div>
+            <div class="col-md-3">
+            
+                <button class="result-btn">Apply for Job</button>
+                <h6> 7 hours ago</h6>
+            </div>
+                </div>
+            </div>`;
+
+            
+    
+              
+          
+        }
+    
+        
+    
+    })
+    
+    .catch(() =>{
+    
+        err.innerHTML = " keyword not found, please try another one"
+    
+    });
+    
+    }
+    
+    
+    
+// adminn.addEventListener("onclick", getVacancies);
+
+
+// getVacancies();
